8e2eae3f1d5c58cad32293ed01c6f8d376c89c85
gelin <gllovejava@163.com>
2019-08-04 02:46:30 +0800

    springCloud 自带的配置中心，客户端

---
+++ config-client/src/main/java/fangfa/net/configclient/ConfigClientController.java
@@ -0,0 +1,27 @@
+package fangfa.net.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: gl
+ * @Email: 110.com
+ * @version: 1.0
+ * @Date: 2019/8/4
+ * @describe:
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${gl}")
+    private String serverPort;
+
+
+    @RequestMapping("/get_config_name")
+    public String getConfigName() {
+        return serverPort;
+    }
+
+
+}
